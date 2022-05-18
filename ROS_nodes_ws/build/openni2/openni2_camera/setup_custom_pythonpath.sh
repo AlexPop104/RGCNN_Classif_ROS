fb2a2ae0e98e886b5257e8ad55f83abe08d0e3af
AlexPop104 <alexandrupop104@gmail.com>
2022-05-18 19:26:44 +0300

    Classification demo ready to use

--- ROS_nodes_ws/build/openni2/openni2_camera/setup_custom_pythonpath.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/alex/Alex_documents/RGCNN_git/Git_folder/ROS_nodes_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH
-exec /usr/bin/python2 "$@"
